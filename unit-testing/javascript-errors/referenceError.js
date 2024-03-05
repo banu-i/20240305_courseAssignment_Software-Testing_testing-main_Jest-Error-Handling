641f4961de3482c119a73ea183f261ebe5fbd12c
banu-i <152266442+banu-i@users.noreply.github.com>
2024-03-05 18:35:04 +0100

    Kursaufgabe20240305-Aufgabe 3:  Error Handle: try, catch, finally

--- unit-testing/javascript-errors/referenceError.js
+++ unit-testing/javascript-errors/referenceError.js
@@ -1,3 +1,11 @@
 // Folgender Code wirft einen ReferenceError
 
-meineLieblingsFunktion();
+try {
+  meineLieblingsFunktion();
+} catch (error) {
+  console.error("ReferenceError behandelt:", error.message);
+} finally {
+  console.log(
+    "Die Ausführung vom Code wurde versucht, aber ist mit einem Fehler!"
+  );
+}
--- unit-testing/javascript-errors/typeError.js
+++ unit-testing/javascript-errors/typeError.js
@@ -1,7 +1,14 @@
 // Folgender Code wirft einen TypeError
 
-const zahl = 1024;
-
-zahl.map((item) => {
-  console.log(item);
-});
+try {
+  const zahl = 1024;
+  zahl.map((item) => {
+    console.log(item);
+  });
+} catch (error) {
+  console.error("TypeError behandelt:", error.message);
+} finally {
+  console.log(
+    "Die Ausführung vom Code wurde versucht, aber ist mit einem Fehler!"
+  );
+}
