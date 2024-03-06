2fd262763fccad3785af0028a2d7b6f2985b8da5
banu-i <152266442+banu-i@users.noreply.github.com>
2024-03-06 19:32:43 +0100

    sync: 20240306_Unterricht: AsynchroneTests ; ErrorHandling ;TestDrivenDevelopment; Mocks : Upload Dateien - Kursaufgabe: 20240306-Status in Bearbeitung

--- unit-testing/asynchroneTest_errorHandling_testDrivenDevelopment_mocks_20240306/AgeFunctions.test.js
+++ unit-testing/asynchroneTest_errorHandling_testDrivenDevelopment_mocks_20240306/AgeFunctions.test.js
@@ -1,7 +1,36 @@
+// Kursaufgabe 20240326 - Aufgabe 2:
+// Ihr sollt in dieser Aufgabe die AgeFunction.test.js so erweitern,
+// dass die Funktion isAdult eine Line-Coverage von 100% hat.
+
+// TEST für ein Alter "größer" oder "gleich" 18
+// TEST für ein Alter "kleiner" als 18
+// TEST für ein "negatives Alter"
+// TEST für ein "nicht-nummerisches Alter"
+
 const { isAdult } = require("./AgeFunctions");
 
 describe("Tests for isAdult Function", () => {
   test("Test for age being 18", () => {
     expect(isAdult(18)).toBe(true);
+  });
+
+  test("TEST für ein Alter größer 18", () => {
+    expect(isAdult(27)).toBe(true);
+  });
+
+  test("TEST für ein Alter kleiner 18", () => {
+    expect(isAdult(16)).toBe(false);
+  });
+
+  test("TEST für ein negatives Alter", () => {
+    expect(() => {
+      isAdult(-27);
+    }).toThrow(TypeError);
+  });
+
+  test("TEST für ein nicht-nummerisches Alter", () => {
+    expect(() => {
+      isAdult("18");
+    }).toThrow(TypeError);
   });
 });
