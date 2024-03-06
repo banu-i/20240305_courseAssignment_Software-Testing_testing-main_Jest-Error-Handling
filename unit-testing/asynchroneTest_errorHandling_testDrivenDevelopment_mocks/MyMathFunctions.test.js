40f02af963e1bde2e743afe5e4e830a112a367aa
banu-i <152266442+banu-i@users.noreply.github.com>
2024-03-06 13:48:32 +0100

    20240306_Unterricht: AsynchroneTests ; ErrorHandling ;TestDrivenDevelopment; Mocks : MyMathFunctions.js und MyMathFunctions.test.js

---
+++ unit-testing/asynchroneTest_errorHandling_testDrivenDevelopment_mocks/MyMathFunctions.test.js
@@ -0,0 +1,18 @@
+const { addNumbers, divideNrs } = require("./MyMathFunctions");
+
+describe("Test Basic Math Add Functions", () => {
+  test("Basic Add Function to pass with Basic input", () => {
+    const result = addNumbers(10, 5);
+    console.log("My Result", result);
+    expect(result).toBe(2);
+  });
+
+  // describe("Test Basic Math Divide Functions", () => {
+  test("TypeError bei Division durch Null", () => {
+    // const result = divideNrs(10, 0);
+    // console.log("MY division result", result);
+    expect(() => {
+      divideNrs(10, 0);
+    }).toThrow(TypeError);
+  });
+});
